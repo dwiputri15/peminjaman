72f13cea44603fad5c84b0a8e582aa7a544d4a84
dwiputri15 <100397747+dwiputri15@users.noreply.github.com>
2022-03-27 00:45:37 +0700

    edit

--- application/views/peminjaman.php
+++ application/views/peminjaman.php
@@ -31,7 +31,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <!--end::Global Theme Styles-->
     <!--begin::Layout Themes(used by all pages)-->
     <!--end::Layout Themes-->
-    <link rel="shortcut icon" href="<?php echo base_url('demo13/dist/assets/') ?>media/logos/favicon.ico" />
+    <link rel="shortcut icon" href=https://1.bp.blogspot.com/-PpH7CcrJIvE/X5WGsZUp6LI/AAAAAAAADRE/VIw1_NyjUC4HUnijuO1r2WBG4tfjpJGbgCLcBGAsYHQ/w1200-h630-p-k-no-nu/Logo%2BPolinema%2B%2528Politeknik%2BNegeri%2BMalang%2529.png />
 </head>
 <!--end::Head-->
 <!--begin::Body-->
@@ -99,13 +99,13 @@ License: You must have a valid license purchased only from themeforest(the above
                             <li class="menu-item" aria-haspopup="true">
                                 <a href="index.html" class="menu-link">
                                     <i class="menu-icon flaticon2-architecture-and-city"></i>
-                                    <span class="menu-text">Export</span>
+                                    <span class="menu-text">Beranda</span>
                                 </a>
                             </li>
                             <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
                                 <a href="javascript:;" class="menu-link menu-toggle">
                                     <i class="menu-icon flaticon2-telegram-logo"></i>
-                                    <span class="menu-text">Actions</span>
+                                    <span class="menu-text">Alat Yang Dipinjam </span>
                                     <i class="menu-arrow"></i>
                                 </a>
 
@@ -113,7 +113,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
                                 <a href="javascript:;" class="menu-link menu-toggle">
                                     <i class="menu-icon flaticon2-laptop"></i>
-                                    <span class="menu-text">Pages</span>
+                                    <span class="menu-text">Kelola Alat Praktikum</span>
                                     <i class="menu-arrow"></i>
                                 </a>
                                 <div class="menu-submenu">
@@ -121,7 +121,7 @@ License: You must have a valid license purchased only from themeforest(the above
                                     <ul class="menu-subnav">
                                         <li class="menu-item menu-item-parent" aria-haspopup="true">
                                             <span class="menu-link">
-                                                <span class="menu-text">Pages</span>
+                                                <span class="menu-text"></span>
                                             </span>
                                         </li>
                                         <li class="menu-item" aria-haspopup="true">
@@ -305,75 +305,9 @@ License: You must have a valid license purchased only from themeforest(the above
                                 <!--begin::Toolbar-->
                                 <div class="d-flex align-items-center">
                                     <!--begin::Actions-->
-                                    <a href="#" class="btn btn-light-primary font-weight-bolder btn-sm">Actions</a>
-                                    <!--end::Actions-->
+
                                     <!--begin::Dropdown-->
-                                    <div class="dropdown dropdown-inline" data-toggle="tooltip" title="Quick actions" data-placement="left">
-                                        <a href="#" class="btn btn-icon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <span class="svg-icon svg-icon-success svg-icon-2x">
-                                                <!--begin::Svg Icon | path:assets/media/svg/icons/Files/File-plus.svg-->
-                                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                                        <polygon points="0 0 24 0 24 24 0 24" />
-                                                        <path d="M5.85714286,2 L13.7364114,2 C14.0910962,2 14.4343066,2.12568431 14.7051108,2.35473959 L19.4686994,6.3839416 C19.8056532,6.66894833 20,7.08787823 20,7.52920201 L20,20.0833333 C20,21.8738751 19.9795521,22 18.1428571,22 L5.85714286,22 C4.02044787,22 4,21.8738751 4,20.0833333 L4,3.91666667 C4,2.12612489 4.02044787,2 5.85714286,2 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
-                                                        <path d="M11,14 L9,14 C8.44771525,14 8,13.5522847 8,13 C8,12.4477153 8.44771525,12 9,12 L11,12 L11,10 C11,9.44771525 11.4477153,9 12,9 C12.5522847,9 13,9.44771525 13,10 L13,12 L15,12 C15.5522847,12 16,12.4477153 16,13 C16,13.5522847 15.5522847,14 15,14 L13,14 L13,16 C13,16.5522847 12.5522847,17 12,17 C11.4477153,17 11,16.5522847 11,16 L11,14 Z" fill="#000000" />
-                                                    </g>
-                                                </svg>
-                                                <!--end::Svg Icon-->
-                                            </span>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-md dropdown-menu-right p-0 m-0">
-                                            <!--begin::Navigation-->
-                                            <ul class="navi navi-hover">
-                                                <li class="navi-header font-weight-bold py-4">
-                                                    <span class="font-size-lg">Choose Label:</span>
-                                                    <i class="flaticon2-information icon-md text-muted" data-toggle="tooltip" data-placement="right" title="Click to learn more..."></i>
-                                                </li>
-                                                <li class="navi-separator mb-3 opacity-70"></li>
-                                                <li class="navi-item">
-                                                    <a href="#" class="navi-link">
-                                                        <span class="navi-text">
-                                                            <span class="label label-xl label-inline label-light-success">Customer</span>
-                                                        </span>
-                                                    </a>
-                                                </li>
-                                                <li class="navi-item">
-                                                    <a href="#" class="navi-link">
-                                                        <span class="navi-text">
-                                                            <span class="label label-xl label-inline label-light-danger">Partner</span>
-                                                        </span>
-                                                    </a>
-                                                </li>
-                                                <li class="navi-item">
-                                                    <a href="#" class="navi-link">
-                                                        <span class="navi-text">
-                                                            <span class="label label-xl label-inline label-light-warning">Suplier</span>
-                                                        </span>
-                                                    </a>
-                                                </li>
-                                                <li class="navi-item">
-                                                    <a href="#" class="navi-link">
-                                                        <span class="navi-text">
-                                                            <span class="label label-xl label-inline label-light-primary">Member</span>
-                                                        </span>
-                                                    </a>
-                                                </li>
-                                                <li class="navi-item">
-                                                    <a href="#" class="navi-link">
-                                                        <span class="navi-text">
-                                                            <span class="label label-xl label-inline label-light-dark">Staff</span>
-                                                        </span>
-                                                    </a>
-                                                </li>
-                                                <li class="navi-separator mt-3 opacity-70"></li>
-                                                <li class="navi-footer py-4">
-                                                    <a class="btn btn-clean font-weight-bold btn-sm" href="#">
-                                                        <i class="ki ki-plus icon-sm"></i>Add new</a>
-                                                </li>
-                                            </ul>
-                                            <!--end::Navigation-->
-                                        </div>
-                                    </div>
+
                                     <!--end::Dropdown-->
                                 </div>
                                 <!--end::Toolbar-->
