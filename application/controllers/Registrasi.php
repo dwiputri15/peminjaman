a36b0c158b37625d9fc133984996fa044de24a84
dwiputri15 <100397747+dwiputri15@users.noreply.github.com>
2022-03-29 11:50:06 +0700

    edit UI

--- application/controllers/Registrasi.php
+++ application/controllers/Registrasi.php
@@ -1,10 +1,11 @@
 <?php
 
-class Registrasi extends CI_Controller {
-    
-    public function index(){
-        
-        $this->load->view('sign_up');
-    }
+class Registrasi extends CI_Controller
+{
 
+    public function index()
+    {
+
+        $this->load->view('login/sign_up');
+    }
 }
