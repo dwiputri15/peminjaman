e58032af786954efc5e9f492d2f3e21691e3691f
dwiputri15 <100397747+dwiputri15@users.noreply.github.com>
2022-04-03 11:27:43 +0700

    ui dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,16 +1,24 @@
 <?php
 
-class Dashboard extends CI_Controller {
-    
-    public function index(){
-        
-        $this->load->view('dashboard');
+class Dashboard extends CI_Controller
+{
+
+    public function index()
+    {
+
+        $this->load->view('template/template_header');
+
+        // view utama
+        $this->load->view('dashboard/dashboard');
+
+        $this->load->view('template/template_footer');
     }
 
 
 
 
-    function buat_pw() {
+    function buat_pw()
+    {
 
 
         echo password_hash("1212", PASSWORD_ARGON2I);
